b88933df5210304ed1701883e9c2eb31577d647f
NarsiBhatii <narsibhati31102@gmail.com>
2024-09-08 21:36:07 +0530

    front-end with backend Jwt and middleware

--- index.js
+++ index.js
@@ -1,52 +1,99 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "harkirat123";
 
 const app = express();
 app.use(express.json());
 
-let users = [];
+const users = [];
 
-app.post("/signup", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  console.log(users);
+function logger(req, res, next) {
+  console.log(req.method + " request came");
+  next();
+}
 
-  if (users.find((user) => user.username === username)) {
-    res.status(400).json({ message: "user already exist" });
-  } else {
-    users.push({
-      username,
-      password,
-    });
-    res.status(200).json({ message: "You are signup" });
-  }
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
-
-app.post("/signin", (req, res) => {
+app.post("/signup", logger, function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
-
-  const token = "jshfhsfshfjsfjshfjsjfsjfhjshf";
-
-  let userFound = false;
-  users = users.map((user) => {
-    if (user.username === username && user.password === password) {
-      userFound = true;
-      return { ...user, token };
-    }
-    return user;
+  users.push({
+    username: username,
+    password: password,
   });
 
-  if (userFound) {
-    res.status(200).json({
-      message: "you are signin",
-      token,
+  // we should check if a user with this username already exists
+
+  res.json({
+    message: "You are signed in",
+  });
+});
+
+app.post("/signin", logger, function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  let foundUser = null;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === username && users[i].password === password) {
+      foundUser = users[i];
+    }
+  }
+
+  if (!foundUser) {
+    res.json({
+      message: "Credentials incorrect",
     });
+    return;
   } else {
-    res.status(400).json({
-      message: "not found",
+    const token = jwt.sign(
+      {
+        username: "raman",
+      },
+      JWT_SECRET
+    );
+    res.header("jwt", token);
+
+    res.header("random", "harkirat");
+
+    res.json({
+      token: token,
     });
   }
 });
+
+function auth(req, res, next) {
+  const token = req.headers.token;
+  const decodedData = jwt.verify(token, JWT_SECRET);
+
+  if (decodedData.username) {
+    req.username = decodedData.username;
+    next();
+  } else {
+    res.json({
+      message: "You are not logged in",
+    });
+  }
+}
+
+app.get("/me", logger, auth, function (req, res) {
+  let foundUser = null;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === req.username) {
+      foundUser = users[i];
+    }
+  }
+
+  res.json({
+    username: foundUser.username,
+    password: foundUser.password,
+  });
+});
+
 app.listen(3000, () => {
-  console.log(users);
+  console.log("express server is listening on port: 3000");
 });
